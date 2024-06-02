46e1ada1aa4db2628cc6a8307468dacabef14089
Phuoc Dinh Thien <dinhthienphuoc1990@gmail.com>
2024-06-02 19:55:45 +0700

    prepareForReview: add reviewer readme

---
+++ FOR_REVIEWER.md
@@ -0,0 +1,3 @@
+I provide the docker-compose file for local setup.
+Just need to run "docker-compose up -d" for local env
+For DB, need to run migrate.
--- config.py
+++ config.py
@@ -9,5 +9,5 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
+# DONE IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/fyyur'
